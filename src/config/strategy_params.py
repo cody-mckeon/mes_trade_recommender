e41cd753526c5dd36254c21dc9c248114d446ce7
cody-mckeon <codymckeon@gmail.com>
2025-05-08 16:00:50 -0700

    Create strategy_params.py
    Feature parameters and RF params

---
+++ src/config/strategy_params.py
@@ -0,0 +1,10 @@
+best_strategy_params = {
+    'confidence_threshold': 0.551569875903487,
+    'atr_mult': 2.4844849418131574,
+    'rr_ratio': 2.439982740786732,
+    'confidence_margin': 0.03572679713640822,
+    'atr_threshold': 79.40292024718671,
+    'max_hold_days': 8
+}
+
+best_rf_params = {'n_estimators': 27, 'max_depth': 2}
